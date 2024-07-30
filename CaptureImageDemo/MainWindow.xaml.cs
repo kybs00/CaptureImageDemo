cd9fba39ac0d31bb653a671e3ea0c97c604592fe
kybs00 <1016724958@qq.com>
2024-07-30 10:27:11 +0800

    整理

--- CaptureImageDemo/MainWindow.xaml.cs
+++ CaptureImageDemo/MainWindow.xaml.cs
@@ -34,6 +34,17 @@ namespace CaptureImageDemo
             CaptureImage.Source = ConvertBitmapToBitmapSource(image);
             Debug.WriteLine(stopwatch.ElapsedMilliseconds);
         }
+        /// <summary>
+        /// 截图屏幕
+        /// </summary>
+        /// <returns></returns>
+        public static Bitmap CaptureScreen()
+        {
+            IntPtr desktopWindow = GetDesktopWindow();
+            //获取窗口位置大小
+            GetWindowRect(desktopWindow, out var lpRect);
+            return CaptureByGdi(desktopWindow, 0d, 0d, lpRect.Width, lpRect.Height);
+        }
         private BitmapSource ConvertBitmapToBitmapSource(Bitmap bitmap)
         {
             using MemoryStream memoryStream = new MemoryStream();
@@ -51,17 +62,6 @@ namespace CaptureImageDemo
             // 确保内存流不会被回收
             bitmapImage.Freeze();
             return bitmapImage;
-        }
-        /// <summary>
-        /// 截图屏幕
-        /// </summary>
-        /// <returns></returns>
-        public static Bitmap CaptureScreen()
-        {
-            IntPtr desktopWindow = GetDesktopWindow();
-            //获取窗口位置大小
-            GetWindowRect(desktopWindow, out var lpRect);
-            return CaptureByGdi(desktopWindow, 0d, 0d, lpRect.Width, lpRect.Height);
         }
         /// <summary>
         /// 截图窗口/屏幕
@@ -87,141 +87,141 @@ namespace CaptureImageDemo
             ReleaseDC(windowIntPtr, windowDc);
             return bitmap;
         }
+    /// <summary>
+    /// 获取桌面窗口
+    /// </summary>
+    /// <returns></returns>
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetDesktopWindow();
+    /// <summary>
+    /// 获取整个窗口的矩形区域
+    /// </summary>
+    /// <returns></returns>
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+    /// <summary>
+    /// 检索整个窗口的设备上下文
+    /// </summary>
+    /// <param name="hWnd">具有要检索的设备上下文的窗口的句柄</param>
+    /// <returns></returns>
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr GetWindowDC(IntPtr hWnd);
+    /// <summary>
+    /// 创建与指定设备兼容的内存设备上下文
+    /// </summary>
+    /// <param name="hdc">现有 DC 的句柄</param>
+    /// <returns>如果函数成功，则返回值是内存 DC 的句柄，否则返回Null</returns>
+    [DllImport("gdi32.dll")]
+    public static extern IntPtr CreateCompatibleDC([In] IntPtr hdc);
+    /// <summary>
+    /// 将对象选择到指定的设备上下文中
+    /// </summary>
+    /// <param name="hdc">DC 的句柄</param>
+    /// <param name="gdiObj">要选择的对象句柄</param>
+    /// <returns>如果函数成功，则返回值是兼容位图 (DDB) 的句柄，否则返回Null</returns>
+    [DllImport("gdi32.dll")]
+    public static extern IntPtr SelectObject([In] IntPtr hdc, [In] IntPtr gdiObj);
+    /// <summary>
+    /// 创建与与指定设备上下文关联的设备的位图
+    /// </summary>
+    /// <param name="hdc">设备上下文的句柄</param>
+    /// <param name="nWidth">位图宽度（以像素为单位）</param>
+    /// <param name="nHeight">位图高度（以像素为单位）</param>
+    /// <returns></returns>
+    [DllImport("gdi32.dll")]
+    public static extern IntPtr CreateCompatibleBitmap([In] IntPtr hdc, int nWidth, int nHeight);
+    /// <summary>
+    /// 执行与从指定源设备上下文到目标设备上下文中的像素矩形对应的颜色数据的位块传输
+    /// </summary>
+    /// <param name="hdcDest">目标设备上下文的句柄</param>
+    /// <param name="xDest">目标矩形左上角的 x 坐标（逻辑单位）</param>
+    /// <param name="yDest">目标矩形左上角的 y 坐标（逻辑单位）</param>
+    /// <param name="wDest">源矩形和目标矩形的宽度（逻辑单位）</param>
+    /// <param name="hDest">源矩形和目标矩形的高度（逻辑单位）</param>
+    /// <param name="hdcSource">源设备上下文的句柄</param>
+    /// <param name="xSrc">源矩形左上角的 x 坐标（逻辑单位）</param>
+    /// <param name="ySrc">源矩形左上角的 y 坐标（逻辑单位）</param>
+    /// <param name="rop">定义如何将源矩形的颜色数据与目标矩形的颜色数据相结合</param>
+    /// <returns></returns>
+    [DllImport("gdi32.dll")]
+    public static extern bool BitBlt(IntPtr hdcDest,
+        int xDest, int yDest, int wDest, int hDest, IntPtr hdcSource,
+        int xSrc, int ySrc, CopyPixelOperation rop);
+    /// <summary>
+    /// 删除逻辑笔、画笔、字体、位图、区域或调色板，释放与对象关联的所有系统资源。
+    /// 删除对象后，指定的句柄将不再有效。
+    /// </summary>
+    /// <param name="hObject"></param>
+    /// <returns></returns>
+    [DllImport("gdi32.dll")]
+    public static extern bool DeleteObject(IntPtr hObject);
+    /// <summary>
+    /// 删除指定的设备上下文
+    /// </summary>
+    /// <param name="hdc">设备上下文的句设备上下文的句</param>
+    /// <returns></returns>
+    [DllImport("gdi32.dll")]
+    public static extern bool DeleteDC([In] IntPtr hdc);
+    /// <summary>
+    /// 释放设备上下文 （DC），释放它以供其他应用程序使用
+    /// </summary>
+    /// <param name="hWnd"></param>
+    /// <param name="hdc"></param>
+    /// <returns></returns>
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool ReleaseDC(IntPtr hWnd, IntPtr hdc);
+
+    /// <summary>
+    /// 定义一个矩形区域。
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
         /// <summary>
-        /// 获取桌面窗口
+        /// 矩形左侧的X坐标。
         /// </summary>
-        /// <returns></returns>
-        [DllImport("user32.dll")]
-        public static extern IntPtr GetDesktopWindow();
-        /// <summary>
-        /// 获取整个窗口的矩形区域
-        /// </summary>
-        /// <returns></returns>
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
-        /// <summary>
-        /// 检索整个窗口的设备上下文
-        /// </summary>
-        /// <param name="hWnd">具有要检索的设备上下文的窗口的句柄</param>
-        /// <returns></returns>
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr GetWindowDC(IntPtr hWnd);
-        /// <summary>
-        /// 创建与指定设备兼容的内存设备上下文
-        /// </summary>
-        /// <param name="hdc">现有 DC 的句柄</param>
-        /// <returns>如果函数成功，则返回值是内存 DC 的句柄，否则返回Null</returns>
-        [DllImport("gdi32.dll")]
-        public static extern IntPtr CreateCompatibleDC([In] IntPtr hdc);
-        /// <summary>
-        /// 将对象选择到指定的设备上下文中
-        /// </summary>
-        /// <param name="hdc">DC 的句柄</param>
-        /// <param name="gdiObj">要选择的对象句柄</param>
-        /// <returns>如果函数成功，则返回值是兼容位图 (DDB) 的句柄，否则返回Null</returns>
-        [DllImport("gdi32.dll")]
-        public static extern IntPtr SelectObject([In] IntPtr hdc, [In] IntPtr gdiObj);
-        /// <summary>
-        /// 创建与与指定设备上下文关联的设备的位图
-        /// </summary>
-        /// <param name="hdc">设备上下文的句柄</param>
-        /// <param name="nWidth">位图宽度（以像素为单位）</param>
-        /// <param name="nHeight">位图高度（以像素为单位）</param>
-        /// <returns></returns>
-        [DllImport("gdi32.dll")]
-        public static extern IntPtr CreateCompatibleBitmap([In] IntPtr hdc, int nWidth, int nHeight);
-        /// <summary>
-        /// 执行与从指定源设备上下文到目标设备上下文中的像素矩形对应的颜色数据的位块传输
-        /// </summary>
-        /// <param name="hdcDest">目标设备上下文的句柄</param>
-        /// <param name="xDest">目标矩形左上角的 x 坐标（逻辑单位）</param>
-        /// <param name="yDest">目标矩形左上角的 y 坐标（逻辑单位）</param>
-        /// <param name="wDest">源矩形和目标矩形的宽度（逻辑单位）</param>
-        /// <param name="hDest">源矩形和目标矩形的高度（逻辑单位）</param>
-        /// <param name="hdcSource">源设备上下文的句柄</param>
-        /// <param name="xSrc">源矩形左上角的 x 坐标（逻辑单位）</param>
-        /// <param name="ySrc">源矩形左上角的 y 坐标（逻辑单位）</param>
-        /// <param name="rop">定义如何将源矩形的颜色数据与目标矩形的颜色数据相结合</param>
-        /// <returns></returns>
-        [DllImport("gdi32.dll")]
-        public static extern bool BitBlt(IntPtr hdcDest,
-            int xDest, int yDest, int wDest, int hDest, IntPtr hdcSource,
-            int xSrc, int ySrc, CopyPixelOperation rop);
-        /// <summary>
-        /// 删除逻辑笔、画笔、字体、位图、区域或调色板，释放与对象关联的所有系统资源。
-        /// 删除对象后，指定的句柄将不再有效。
-        /// </summary>
-        /// <param name="hObject"></param>
-        /// <returns></returns>
-        [DllImport("gdi32.dll")]
-        public static extern bool DeleteObject(IntPtr hObject);
-        /// <summary>
-        /// 删除指定的设备上下文
-        /// </summary>
-        /// <param name="hdc">设备上下文的句设备上下文的句</param>
-        /// <returns></returns>
-        [DllImport("gdi32.dll")]
-        public static extern bool DeleteDC([In] IntPtr hdc);
-        /// <summary>
-        /// 释放设备上下文 （DC），释放它以供其他应用程序使用
-        /// </summary>
-        /// <param name="hWnd"></param>
-        /// <param name="hdc"></param>
-        /// <returns></returns>
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool ReleaseDC(IntPtr hWnd, IntPtr hdc);
+        public int Left;
 
         /// <summary>
-        /// 定义一个矩形区域。
+        /// 矩形顶部的Y坐标。
         /// </summary>
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
+        public int Top;
+
+        /// <summary>
+        /// 矩形右侧的X坐标。
+        /// </summary>
+        public int Right;
+
+        /// <summary>
+        /// 矩形底部的Y坐标。
+        /// </summary>
+        public int Bottom;
+
+        /// <summary>
+        /// 获取矩形的宽度。
+        /// </summary>
+        public int Width => Right - Left;
+
+        /// <summary>
+        /// 获取矩形的高度。
+        /// </summary>
+        public int Height => Bottom - Top;
+
+        /// <summary>
+        /// 初始化一个新的矩形。
+        /// </summary>
+        /// <param name="left">矩形左侧的X坐标。</param>
+        /// <param name="top">矩形顶部的Y坐标。</param>
+        /// <param name="right">矩形右侧的X坐标。</param>
+        /// <param name="bottom">矩形底部的Y坐标。</param>
+        public RECT(int left, int top, int right, int bottom)
         {
-            /// <summary>
-            /// 矩形左侧的X坐标。
-            /// </summary>
-            public int Left;
-
-            /// <summary>
-            /// 矩形顶部的Y坐标。
-            /// </summary>
-            public int Top;
-
-            /// <summary>
-            /// 矩形右侧的X坐标。
-            /// </summary>
-            public int Right;
-
-            /// <summary>
-            /// 矩形底部的Y坐标。
-            /// </summary>
-            public int Bottom;
-
-            /// <summary>
-            /// 获取矩形的宽度。
-            /// </summary>
-            public int Width => Right - Left;
-
-            /// <summary>
-            /// 获取矩形的高度。
-            /// </summary>
-            public int Height => Bottom - Top;
-
-            /// <summary>
-            /// 初始化一个新的矩形。
-            /// </summary>
-            /// <param name="left">矩形左侧的X坐标。</param>
-            /// <param name="top">矩形顶部的Y坐标。</param>
-            /// <param name="right">矩形右侧的X坐标。</param>
-            /// <param name="bottom">矩形底部的Y坐标。</param>
-            public RECT(int left, int top, int right, int bottom)
-            {
-                Left = left;
-                Top = top;
-                Right = right;
-                Bottom = bottom;
-            }
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
         }
+    }
 
         private void GraphicsCaptureButton_OnClick(object sender, RoutedEventArgs e)
         {
